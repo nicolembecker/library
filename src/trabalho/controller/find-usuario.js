8c5c9c42ff3feeb57eea68ae822b0bd36c39fe58
Nicole Marques <61321129+nicolembecker@users.noreply.github.com>
2021-09-21 23:15:45 -0300

    save trabalho

---
+++ src/trabalho/controller/find-usuario.js
@@ -0,0 +1,49 @@
+function selectAuthor() {
+
+    $('.alert').click(function(e) {
+        e.preventDefault()
+        let id = $(this).attr('id')
+        let nome = $(this).attr('data-name')
+        $('#result').append(`
+            <div class="alert alert-primary">${nome}</div>
+            <input type="hidden" name="USUARIO_IDUSUARIO" value="${id}" />
+        `)
+        $('#' + id).hide()
+    })
+
+}
+
+$(document).ready(function() {
+
+    $('#AUTOR').keyup(function(e) {
+        e.preventDefault()
+
+        let NOME = `NOME=${$(this).val()}`
+
+        $('#autores').empty()
+
+        if ($(this).val().length >= 3) {
+
+            $('#autores').empty()
+
+            $.ajax({
+                dataType: 'json',
+                type: 'POST',
+                assync: true,
+                data: NOME,
+                url: 'src/usuario/model/find-usuario.php',
+                success: function(dados) {
+                    for (const dado of dados) {
+                        $('#autores').append(`<input type="text" name="" id="" class="form-control" value="${dado.NOME}" disabled>`)
+                        $('#autores').append(`<div id="${dado.IDUSUARIO}" data-name="${dado.NOME}" class="alert alert-secondary">${dado.NOME}</div>`)
+                    }
+                    selectAuthor()
+                }
+            })
+
+        } else {
+            $('#autores').empty()
+        }
+
+    })
+})
